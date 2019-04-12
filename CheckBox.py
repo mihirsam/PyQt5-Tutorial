c74396dafab3dc9b64af288c4108d55c317a4222
Mihir <xenon.of.x@gmail.com>
2019-04-13 01:15:01 +0530

    Complete CheckBox Program

--- CheckBox.py
+++ CheckBox.py
@@ -24,11 +24,13 @@ class Window(QDialog):
         self.setWindowIcon(QtGui.QIcon(self.IconName))
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.label = QLabel(self)
 
         self.CreateCheckBox()
         vbox = QVBoxLayout()
         vbox.addWidget(self.groupBox)
-        
+        vbox.addWidget(self.label)
+
         self.setLayout(vbox)
         self.show()
 
@@ -37,14 +39,37 @@ class Window(QDialog):
         self.groupBox.setFont(QtGui.QFont("Sanserif", 10))
         hboxLayout = QHBoxLayout()
 
-        self.check1 = QCheckBox("Python")
+        self.check1 = QCheckBox("Java")
         self.check1.setIcon(QtGui.QIcon(self.IconName))
         self.check1.setIconSize(QtCore.QSize(40, 40))
+        self.check1.toggled.connect(self.OnCheckBox)
+
+        self.check2 = QCheckBox("C++")
+        self.check2.setIcon(QtGui.QIcon(self.IconName))
+        self.check2.setIconSize(QtCore.QSize(40, 40))
+        self.check2.setFont(QtGui.QFont("Sanserif", 10))
+        self.check2.toggled.connect(self.OnCheckBox)
+
+        self.check3 = QCheckBox("Python")
+        self.check3.setIcon(QtGui.QIcon(self.IconName))
+        self.check3.setIconSize(QtCore.QSize(40, 40))
+        self.check3.toggled.connect(self.OnCheckBox)
 
         hboxLayout.addWidget(self.check1)
+        hboxLayout.addWidget(self.check2)
+        hboxLayout.addWidget(self.check3)
 
         self.groupBox.setLayout(hboxLayout)
 
+    def OnCheckBox(self):
+        if self.check1.isChecked():
+            self.label.setText("You have selected : " + self.check1.text())
+
+        elif self.check2.isChecked():
+            self.label.setText("You have selected : " + self.check2.text())
+
+        elif self.check3.isChecked():
+            self.label.setText("You have selected : " + self.check3.text())
 
 
 if __name__ == "__main__":
