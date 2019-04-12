e86f6c91bd71d8ceb1c5b445ff75f1f9f5c6681e
Mihir <xenon.of.x@gmail.com>
2019-04-13 04:43:55 +0530

    modify signal and slots

--- SignalAndSlots.py
+++ SignalAndSlots.py
@@ -1,9 +1,13 @@
-from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow
+from PyQt5.QtWidgets import QHBoxLayout, QApplication, QPushButton, QMainWindow
 import sys
 from PyQt5 import QtGui
 from PyQt5.QtCore import QRect
 from PyQt5 import QtCore
+from functools import partial
 
+def PButton(string):
+        print(string)
+        
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -20,8 +24,7 @@ class Window(QMainWindow):
         self.setGeometry(left, top, width, height)
 
         self.CreateButton()
-        self.show()
-
+        
 
     def CreateButton(self):
         button = QPushButton("Click Me", self)
@@ -32,9 +35,26 @@ class Window(QMainWindow):
         button.setToolTip("<h2>This is click me button!<h2>")
         button.clicked.connect(self.ButtonAction)
 
+        buttonTwo = QPushButton("Partial", self)
+        #button.move(50, 50)
+        buttonTwo.setGeometry(QRect(100, 200, 111, 28))
+        buttonTwo.setIcon(QtGui.QIcon("Icon/python.png"))
+        buttonTwo.setIconSize(QtCore.QSize(30, 30))
+        buttonTwo.setToolTip("<h2>This is click me button!<h2>")
+        buttonTwo.clicked.connect(partial(PButton, "This is Partial!"))
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(button)
+        hbox.addWidget(buttonTwo)
+
+        self.setLayout(hbox)
+        self.show()
+
     def ButtonAction(self):
          print("Hello World")
-         sys.exit()
+         #sys.exit()
+
+    
 
 if __name__ == "__main__":
     App = QApplication(sys.argv)
